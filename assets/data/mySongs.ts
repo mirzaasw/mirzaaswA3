a7e0b6cff51d09578023a57a9b23fc3714d837c0
mirzaasw <52796909+mirzaasw@users.noreply.github.com>
2019-07-20 17:33:13 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,9 @@
+import {Songs} from '../../app/songInfo';
+
+export const MYSONGS: Songs[]=[
+    
+    {songName:'Rip and Tear', artist:'Bethesda ', genre:'Rock?', yearReleased:'2017',picture:'../../assets/images/rip and tear.jpg'},
+    {songName:'Money Money Money', artist:'Abba', genre:'$$$$$', yearReleased:'1976',picture:'../../assets/images/money.jpg'},
+    {songName:'Sit and Sleep', artist:'Pete Thomas', genre:'Comfy', yearReleased:'Dont know',picture:'../../assets/images/moon.jpg'},
+    {songName:'Here comes the sun', artist:'The beetles', genre:'Rock?', yearReleased:'1969',picture:'../../assets/images/sun.jpg'}
+];
